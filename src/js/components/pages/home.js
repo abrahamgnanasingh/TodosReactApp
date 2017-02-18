aad10aee80345f4ff09ffcecab44691113c5100d
Abraham Gnanasingh <abraham1991jpr@gmail.com>
2017-02-19 01:22:44 +0530

    Added some RxJS code in home page

--- src/js/components/pages/home.js
+++ src/js/components/pages/home.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Rx from 'rxjs/Rx';
 import { Glyphicon } from 'react-bootstrap';
+import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 
 class Home extends Component {
 
+	componentDidMount() {
+		var clickMeBtn = ReactDOM.findDOMNode(this.refs.clickMe).firstChild;
+		Rx.Observable.fromEvent(clickMeBtn, 'click')
+			.subscribe(ev => console.log(ev));
+	}
+
 	render() {
 		return (
 			<div>
-				<RaisedButton label="Click Me!" />
+				<TextField
+					hintText="Hey You"
+					floatingLabelText="Type Something"
+				/>
+				<RaisedButton ref="clickMe" label="Click Me!" />
 				<Glyphicon glyph="user" />
 			</div>
 		)
